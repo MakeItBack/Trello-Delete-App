020df5107630217837658510306898c194f253e2
Mike Back <mikeback75@hotmail.com>
2021-07-26 16:01:37 +1000

    Basic function and styling (no delete)

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
-async function getBoards(){
-    const res = await axios.get(`https://api.trello.com/1/members/me/boards?fields=name,id,starred&key=${myKey}&token=${myToken}`);
-    const main = document.querySelector('#boardList');
-  for (const board of res.data){
-      console.log(board)
-      main.innerHTML += 
-        `<li>${board.starred ? `<input type="checkbox" disabled>` : `<input type="checkbox">`}
-        ${board.name}
-        ${board.starred ? " ⭐" : ""}
-        <span>ID: ${board.id}</span></li>`
-  }
+let num = 0;
+const delBtn = document.querySelector("#delete");
+// delBtn.addEventListener("click", confirmBox);
+const confirmMsg = document.querySelector("#confirmText");
+const ConfirmBox = document.querySelector("#confirm");
+
+async function getBoards() {
+   const res = await axios.get(
+      `https://api.trello.com/1/members/me/boards?fields=name,id,starred&key=${myKey}&token=${myToken}`
+   );
+   const list = document.querySelector("#boardList");
+   for (const board of res.data) {
+      // console.log(board);
+      list.innerHTML +=
+         // ${board.starred ? `<input type="checkbox" disabled>` : `<input type="checkbox">`}
+         `<li 
+            class=${board.starred ? "star" : "noStar"}
+            id=${board.id}
+          >
+            ${board.name}
+         ${board.starred ? " ⭐" : ""}
+        <span>ID: ${board.id}</span></li>`;
+   }
+   //  Once the list has finished rendering, add the event listeners
+   listen();
 }
 getBoards();
 
+function listen() {
+   // Select all the list elements with the class of nostar
+   const listElements = document.querySelectorAll("li.noStar");
+   //  Loop over the nodelist and add an event listener to all that toggles the selected class
+   for (const li of listElements) {
+      li.addEventListener("click", function () {
+         this.classList.toggle("selected");
+         countSelected();
+         // Update button text to display number selected
+         delBtn.textContent = `Delete ${num} Selected`;
+      });
+   }
+}
+
+// Count how many boards are selected
+function countSelected() {
+   const selected = document.querySelectorAll("li.selected");
+   num = selected.length;
+}
+
+// Display the confirmation box
+// function confirmBox() {
+//    confirmMsg.textContent = `Are you sure you want to delete these ${num} Trello boards?`;
+//    confirmBox.classList.remove("hide");
+// }
+
 // TO DO
-// when the delete button is clicked, loop through the checked items and delete them
+// when the delete button is clicked create an array with the IDs of the selected boards
+
+// Add an safety message = eg are you sure you want to delete <num> boards?
+
+// loop through the checked items and delete them
+
 // add the IDs to an array first and then loop over the array?
-// Add an safety message = eg are you sure you want to delete 12 boards?
-
-
-
 
 // To delete a board
 // `https://api.trello.com/1/boards/${boardId}?key=${myKey}&token=${myToken}`
